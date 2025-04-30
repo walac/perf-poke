110f11d5ea9a825361f372f221b444e151dd6c81
Tomas Glozar <tglozar@redhat.com>
2025-04-30 11:08:05 +0200

    Add messages to measurement.sh
    Helpful to collect data from system on which measurement is done

--- measurement.sh
+++ measurement.sh
@@ -3,9 +3,28 @@
 [ -z ${THRESHOLD+x} ] && { echo "THRESHOLD not be set" >&2; exit 1; }
 [ -z ${DURATION+x} ] && { echo "DURATION not be set" >&2; exit 1; }
 
-chrt -f 10 taskset -c 0 perf $PERF_ARGS -- sleep $DURATION & PERF_PID=$!
-chrt -f 5 taskset -c 0 ./perf_poke $PERF_PID $THRESHOLD & POKE_PID=$!
+echo "RHEL-86425 perf-poke measurement, running at $(date)"
+echo
+echo "Kernel version: $(uname -r)"
+perf --version
+echo "/proc/cmdline:"
+cat /proc/cmdline
+echo
 
+echo "Starting perf with arguments: $PERF_ARGS"
+chrt -f 10 taskset -c 0 perf $PERF_ARGS -- sleep $DURATION & PERF_PID=$!
+echo "perf running, PID $PERF_PID"
+echo "Starting perf_poke with perf PID: $PERF_PID and threshold: $THRESHOLD ns"
+chrt -f 5 taskset -c 0 ./perf_poke $PERF_PID $THRESHOLD & POKE_PID=$!
+echo "perf_poke running, PID $POKE_PID"
+echo
+
+echo "...waiting on measurement to end"
 wait $PERF_PID
+echo
+echo "Measurement ended, stopping perf_poke..."
 kill -INT $POKE_PID
 wait $POKE_PID
+echo "perf_poke stopped"
+echo
+echo "Please collect perf data from container if running inside one"
