136e2d0e070a9a846f10e7b0a1b2d85f5726e3be
Tomas Glozar <tglozar@redhat.com>
2025-05-02 16:34:42 +0200

    Collect kcore data separately

--- measurement.sh
+++ measurement.sh
@@ -3,6 +3,7 @@
 [ -z ${THRESHOLD+x} ] && { echo "THRESHOLD not be set" >&2; exit 1; }
 [ -z ${DURATION+x} ] && { echo "DURATION not be set" >&2; exit 1; }
 
+# Print useful information
 echo "RHEL-86425 perf-poke measurement, running at $(date)"
 echo
 echo "Kernel version: $(uname -r)"
@@ -11,16 +12,24 @@ echo "/proc/cmdline:"
 cat /proc/cmdline
 echo
 
+# Start perf first, perf_poke needs its PID
 echo "Starting perf with arguments: $PERF_ARGS"
-chrt -f 10 taskset -c 0 perf $PERF_ARGS -- sleep $DURATION & PERF_PID=$!
+chrt -f 10 taskset -c 0 perf $PERF_ARGS -o perf.data.2 -- sleep $DURATION & PERF_PID=$!
 echo "perf running, PID $PERF_PID"
+
+# Wait for five seconds for perf to initialize
 sleep 5
 ps -p $PERF_PID > /dev/null || { echo "perf not running, aborting" >&2; exit 1; }
 
+# Start perf_poke
 echo "Starting perf_poke with perf PID: $PERF_PID and threshold: $THRESHOLD ns"
 chrt -f 5 taskset -c 0 ./perf_poke $PERF_PID $THRESHOLD & POKE_PID=$!
 echo "perf_poke running, PID $POKE_PID"
 echo
+
+# Collect kcore after perf_poke's BPF program is JITed
+sleep 5
+perf record --no-samples --kcore true
 
 echo "...waiting on measurement to end"
 ps -p $PERF_PID > /dev/null && wait $PERF_PID
@@ -30,8 +39,12 @@ echo "Measurement ended, stopping perf_poke..."
 kill -INT $POKE_PID
 ps -p $POKE_PID > /dev/null && wait $POKE_PID
 
-echo "perf_poke stopped"
+echo "perf_poke stopped, creating an archive from perf data"
+mv perf.data.2 perf.data/data
+tar -czf perf_data.tar.gz perf.data/
+
 echo
+echo "perf data is collected as perf_data.tar.gz"
 echo "Please collect perf data from container if running inside one"
 echo "After the data is collected, this script can be stopped"
 sleep infinity
